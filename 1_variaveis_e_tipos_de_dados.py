3e52d82d624843b2a59185085fe867ed6b9ae871
Irene <irenesilvafranca2016@gmail.com>
2024-01-08 10:04:38 -0300

    passo a passo

---
+++ 1_variaveis_e_tipos_de_dados.py
@@ -0,0 +1,25 @@
+# Variáveis e tipos de dados
+
+# ->Receber dados
+# ->Armazenar dados
+# ->Processar dados
+# ->Mostrar dados Processados
+
+# ->Dados Básicos
+# -> int (inteiro) -10 - 100000 10 0 1
+# -> float (deciamal) -10.1 - 10.08   20.65
+# -> str (string) cadeia de caracteres  "Irene Silva" 'Agatha' '10'
+# -> boll (Boleano) True False
+
+idade = 21.2
+
+
+#print(idade)
+#idade = True
+#print(type(idade))
+
+idade = 21
+idade = 22
+idade = 23
+idade = 'Irene Franssa'
+print(idade)
