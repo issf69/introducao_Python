8d7e2d3703c45e8efec8f7249c36f54dba557a51
Irene <irenesilvafranca2016@gmail.com>
2024-01-10 08:34:42 -0300

    operadores.py

---
+++ 2_entrada_e_saida_de_dados.py
@@ -0,0 +1,13 @@
+# Entrada e saída de dados
+
+'''idade = 21
+nome = 'Irene'
+
+#print(f'Sua idade é: {idade}')
+
+print(f"Olá: {nome}', sua idade é {idade} anos.")'''
+
+idade = input("Digite a sua idade: \n ")
+
+print(f'A sua idade é {idade} anos')
+
---
+++ 4_operadores_aritmeticos.py
@@ -0,0 +1,29 @@
+# + - * /  ** % //  raiz quadrada que é math.sqrt
+# + representa soma
+# - representa subtração
+# * representa multiplicação
+# / representa divisão
+# ** representa Exponenciação
+# % representa  modulo   é resto de divisão interiro
+# // representa  divisão por interiro não aceita que resultado seja ponto flutuante
+# math.sqrt representa   riz quadrada
+
+#operacoes = 2 + 2
+#operacoes = 2 - 2
+#operacoes = 2 * 3
+#operacoes = 2 / 3
+#operacoes = 2 ** 3
+#operacoes = 2 % 3
+#operacoes = 2 // 3
+#operacoes = 25 ** 1/2
+
+#operacoes = 25 ** (1/2)
+#operacoes = 25 ** (0.5)
+#operacoes = 25 ** 0.5
+import math
+#operacoes = math.sqrt(25)
+#operacoes = 5 + 5 - 2 * 3
+#operacoes = (5 + 5 - 2) * 3
+#operacoes = (5 + 5 - 2) ** 3
+operacoes = 5 + 5 - 2 ** 3
+print(operacoes)
