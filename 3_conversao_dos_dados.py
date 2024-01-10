a68fc5460aba06978c9c88687a27357f9ed43b4d
Irene <irenesilvafranca2016@gmail.com>
2024-01-10 08:35:08 -0300

    conversao.py

---
+++ 3_conversao_dos_dados.py
@@ -0,0 +1,23 @@
+#3_conversao_dos_dados.py
+#Inteiro
+#idade = 21
+
+#idade = "21"
+#conjunto de caracteres String quando coloca aspas duplas
+#tipo_de_dado = type (idade)
+
+#print(tipo_de_dado)
+#pode fazer de outra forma
+#idade = 21
+#idade = 21.5
+#idade = "21"
+#idade = int(idade)
+
+#idade = 21
+#idade = str(idade)
+
+idade = 21
+idade = float(idade)
+
+print (idade)
+print (type(idade))
